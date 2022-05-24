055e789b828605c0a6e70d2b87777ecc2b70c015
LuisAJP <luisjaramillopulido@gmail.com>
2022-05-24 13:49:50 +0200

    agregado proyecto de tienda musica DAO

---
+++ MusicaliaDAO/src/Integracion/Clientes/DAOCliente.java
@@ -0,0 +1,29 @@
+/**
+ * Luis Jaramillo
+ */
+package Integracion.Clientes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Negocio.Clientes.TransferCliente;
+
+
+public interface DAOCliente {
+	
+	public Integer altaClienteDAO(TransferCliente datos);
+
+	
+	public Integer bajaClienteDAO(Integer datos);
+
+	
+	public Integer modificarClienteDAO(TransferCliente datos);
+
+	
+	public TransferCliente consultarClienteDAO(String dni);
+
+	
+	public TransferCliente consultarClienteDAO(Integer datos);
+
+	public List<TransferCliente> listarClientesDAO();
+}
